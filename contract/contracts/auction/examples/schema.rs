a07f0c43b678ac20ef0d73399abce05ccddfff0c
eLeeTe <sascha.dev@proton.me>
2024-05-10 20:51:41 +0800

    exporting schema

---
+++ contract/contracts/auction/examples/schema.rs
@@ -0,0 +1,13 @@
+use cosmwasm_schema::write_api;
+
+use marketplace::auction::{Cw721HookMsg, ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+// use cw2981_royalties::{QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        // query: QueryMsg,
+    }
+}
